c55e20f1622e15428de2d0aa36358bca6dab22de
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-12-04 19:12:56 +0100

    Progress on project base/setup

---
+++ src/GOICore.cs
@@ -0,0 +1,35 @@
+﻿using Oxide.Core.Plugins;
+
+namespace Oxide.GettingOverIt
+{
+    public class GOICore : CSPlugin
+    {
+        private bool initialized;
+
+        public GOICore()
+        {
+            Title = "Getting Over It with Bennett Foddy";
+            Author = "Skipcast";
+            IsCorePlugin = true;
+        }
+
+        [HookMethod("Init")]
+        private void Init()
+        {
+            if (initialized)
+                return;
+
+            initialized = true;
+        }
+
+        [HookMethod("OnPluginLoaded")]
+        private void OnPluginLoaded(Plugin plugin)
+        {
+            if (initialized)
+            {
+                // Call OnGameInitialized on hotloaded plugins.
+                plugin.CallHook("OnGameInitialized");
+            }
+        }
+    }
+}
---
+++ src/GOIExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using Oxide.Core;
+using Oxide.Core.Extensions;
+
+namespace Oxide.GettingOverIt
+{
+    public class GOIExtension : Extension
+    {
+        internal static readonly Assembly Assembly = Assembly.GetExecutingAssembly();
+        internal static readonly AssemblyName AssemblyName = Assembly.GetName();
+        internal static readonly VersionNumber AssemblyVersion = new VersionNumber(AssemblyName.Version.Major, AssemblyName.Version.Minor, AssemblyName.Version.Build);
+        internal static readonly string AssemblyAuthors = ((AssemblyCompanyAttribute)Attribute.GetCustomAttribute(Assembly, typeof(AssemblyCompanyAttribute), false)).Company;
+        
+        public GOIExtension(ExtensionManager manager) : base(manager)
+        {
+        }
+
+        public override string Name => "Getting Over It with Bennett Foddy";
+        public override string Author => AssemblyAuthors;
+        public override VersionNumber Version => AssemblyVersion;
+        public override bool IsGameExtension => true;
+
+        public override string[] WhitelistAssemblies => new string[]
+        {
+            "Assembly-CSharp", "Assembly-CSharp-firstpass", "mscorlib", "Oxide.Core", "System", "System.Core", "UnityEngine", "UnityEngine.UI"
+        };
+
+        public override string[] WhitelistNamespaces => new string[]
+        {
+            "Steamworks", "System.Collections", "System.Security.Cryptography", "System.Text", "UnityEngine"
+        };
+
+        public override void OnModLoad()
+        {
+            if (Interface.Oxide.EnableConsole())
+            {
+
+            }
+        }
+    }
+}
