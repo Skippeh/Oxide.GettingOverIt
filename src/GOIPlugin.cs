62c1de502adf351248dfdd4167d12629a4e9fdc2
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-12-05 17:55:23 +0100

    Added virtual Tick method on GOIPlugin

--- src/GOIPlugin.cs
+++ src/GOIPlugin.cs
@@ -1,11 +1,23 @@
 ﻿// Currently not using Oxide.CSharp (CSharpPlugin) because the game targets .NET 2.0 subset (which isn't supported).
 
+using Oxide.Core;
+using Oxide.Core.Libraries;
 using Oxide.Core.Plugins;
 
 namespace Oxide.GettingOverIt
 {
     public abstract class GOIPlugin : CSPlugin
     {
+        protected Timer Timer { get; private set; }
 
+        protected GOIPlugin()
+        {
+            Timer = Interface.Oxide.GetLibrary<Timer>();
+            Timer.Repeat(0, 0, Tick, this);
+        }
+
+        protected virtual void Tick()
+        {
+        }
     }
 }
