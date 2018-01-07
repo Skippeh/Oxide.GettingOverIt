dad5255c89b6c3cf296d9694c04949c538e49b12
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-01-07 23:45:20 +0100

    Removed code from TestPlugin

--- src/Plugins/TestPlugin.cs
+++ src/Plugins/TestPlugin.cs
@@ -1,12 +1,4 @@
-﻿using System.Linq;
-using Oxide.Core;
-using Oxide.Core.Plugins;
-using Oxide.GettingOverIt.Types;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-
-namespace Oxide.GettingOverIt.Plugins
+﻿namespace Oxide.GettingOverIt.Plugins
 {
     public class TestPlugin : GOIPlugin
     {
@@ -14,30 +6,6 @@ namespace Oxide.GettingOverIt.Plugins
         {
             Author = "Skipcast";
             Title = "Test plugin";
-        }
-
-        [HookMethod("Init")]
-        private void OnInit()
-        {
-            Interface.Oxide.LogDebug("Init done");
-        }
-
-        [HookMethod("OnGameQuit")]
-        private void OnGameQuit()
-        {
-            Interface.Oxide.LogDebug("Game quitting");
-        }
-
-        [HookMethod("OnSceneChanged")]
-        private void OnSceneChanged(SceneType sceneType, Scene scene)
-        {
-            Interface.Oxide.LogDebug($"Scene changed to {scene.name} ({scene.buildIndex}).");
-        }
-        
-        [HookMethod("OnLoadGame")]
-        private void OnLoadGame(Saviour saviour, SaveState saveState)
-        {
-            Interface.Oxide.LogDebug($"Load game: {saveState}.");
         }
     }
 }
